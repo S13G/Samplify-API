e6943a61dff4f12f80f183f549dce96264dc63cb
s13g <ayflix0@gmail.com>
2023-06-03 17:13:50 +0100

    added deployment link

--- Samplify/production.py
+++ Samplify/production.py
@@ -4,6 +4,6 @@ SECRET_KEY = config("SECRET_KEY")
 
 DEBUG = config("DEBUG", False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["samplify.up.railway.app"]
 
 CSRF_TRUSTED_ORIGINS = ["https://" + host for host in ALLOWED_HOSTS]
